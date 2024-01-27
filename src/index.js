714c75e9c6971cbef12ec44cd927af9ae0a5fd2d
Muhammad Naufal Imam Purwadinata <130852570+naufalprwd@users.noreply.github.com>
2024-01-27 12:29:27 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDFUrYFJ6iRK22VTw34PeoAQW_m8UWgBHw",
+    authDomain: "uas-bdd-teori-kelompok.firebaseapp.com",
+    projectId: "uas-bdd-teori-kelompok",
+    storageBucket: "uas-bdd-teori-kelompok.appspot.com",
+    messagingSenderId: "242970064386",
+    appId: "1:242970064386:web:9a4e2a54c57b4b49bc991b"
+  };
+
+initializeApp(firebaseConfig)
