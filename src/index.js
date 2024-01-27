d7bf63611993a47c97be2bcf3db27ba6337165a0
Bintng12 <mhdbintang12@gmail.com>
2024-01-27 16:27:24 +0700

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import { initializeApp } from 'firebase/app'
+import {
+  getFirestore, collection, onSnapshot,
+  addDoc, deleteDoc, doc
+} from 'firebase/firestore'
 
 const firebaseConfig = {
     apiKey: "AIzaSyDFUrYFJ6iRK22VTw34PeoAQW_m8UWgBHw",
@@ -9,4 +13,50 @@ const firebaseConfig = {
     appId: "1:242970064386:web:9a4e2a54c57b4b49bc991b"
   };
 
+
+ // init firebase app 
 initializeApp(firebaseConfig)
+
+//  init services
+const db = getFirestore()
+
+// collection ref
+const colref = collection(db, 'books')
+
+// real time collection data
+  onSnapshot(colref, (snapshot) => {
+    let books = []
+    snapshot.docs.forEach((doc) => {
+      books.push({...doc.data(), id: doc.id })
+    })
+    console.log(books)
+  })
+
+  // adding documents
+  const addBookFrom = document.querySelector('.add')
+  addBookFrom.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    addDoc(colref, {
+      title: addBookFrom.title.value,
+      author: addBookFrom.author.value,
+    })
+    .then(() => {
+      addBookFrom.reset()
+    })
+
+  })
+
+  // deleting documents
+  const deleteBookFrom = document.querySelector('.delete')
+  deleteBookFrom.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const docref = doc(db, 'books', deleteBookFrom.id.value)
+
+    deleteDoc(docref)
+    .then(() => {
+      deleteBookFrom.reset()
+    })
+
+  })
